6aa93612049979dde60ee25c6eb84ff3e1860519
Tigro <easterlingme95@gmail.com>
2023-02-25 15:47:56 -0600

    started html creating functions plus employee rendering

---
+++ library/htmlMaker.js
@@ -0,0 +1,24 @@
+const path = require("path");
+const fs = require("fs");
+
+const templatesDir = path.resolve(__dirname, "../templates");
+
+const render = employees => {
+  const html = [];
+
+  html.push((employees
+    .filter(employee => employee.getRole() === "Manager")
+    .map(manager => renderManager(manager)))
+    .join(""));
+  html.push((employees
+    .filter(employee => employee.getRole() === "Engineer")
+    .map(engineer => renderEngineer(engineer)))
+    .join(""));
+  html.push((employees
+    .filter(employee => employee.getRole() === "Intern")
+    .map(intern => renderIntern(intern)))
+    .join(""));
+
+  return renderMain(html.join(""));
+
+};
