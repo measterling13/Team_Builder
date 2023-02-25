ff75d4546421a7f4ea5a7c4c34bc2a7ec4aef4cc
Tigro <easterlingme95@gmail.com>
2023-02-25 16:08:25 -0600

    added the team creation function

--- index.js
+++ index.js
@@ -10,4 +10,105 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./library/htmlMaker");
 
-const employees = []
+const employees = [];
+
+function createTeam() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "What is the name of the employee you would like to add?",
+      },
+
+      {
+        type: "input",
+        name: "id",
+        message: "What is the ID# of this employee?",
+      },
+
+      {
+        type: "input",
+        name: "email",
+        message: "What is this employee's email address?",
+        validate: (input) =>
+          /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(input),
+      },
+
+      {
+        type: "list",
+        name: "role",
+        message: "What is this employee's position?",
+        choices: ["Manager", "Engineer", "Intern"],
+      },
+    ])
+    .then((answers) => {
+      switch (answers.role) {
+        case "Manager":
+          inquirer
+            .prompt([
+              {
+                type: "input",
+                name: "officeNumber",
+                message: "What is this manager's office number?",
+              },
+            ])
+            .then((answers2) => {
+              const manager = new Manager(
+                answers.name,
+                answers.id,
+                answers.email,
+                answers2.officeNumber
+              );
+              employees.push(manager);
+
+              continueTeam();
+            });
+          break;
+
+        case "Engineer":
+          inquirer
+            .prompt([
+              {
+                type: "input",
+                name: "github",
+                message: "What is this engineer's github profile link?",
+              },
+            ])
+            .then((answers2) => {
+              const engineer = new Engineer(
+                answers.name,
+                answers.id,
+                answers.email,
+                answers2.github
+              );
+              employees.push(engineer);
+
+              continueTeam();
+            });
+          break;
+
+        case "Intern":
+          inquirer
+            .prompt([
+              {
+                type: "input",
+                name: "school",
+                message: "What school does/did this intern go to?",
+              },
+            ])
+            .then((answers2) => {
+              const intern = new Intern(
+                answers.name,
+                answers.id,
+                answers.email,
+                answers2.school
+              );
+              employees.push(intern);
+
+              continueTeam();
+            });
+          break;
+      }
+    });
+}
