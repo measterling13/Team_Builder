1ea0873cb6a6e369434bb3cc1b7d0b99559552d0
Tigro <easterlingme95@gmail.com>
2023-02-25 16:04:26 -0600

    added basic model requirements and packages needed

---
+++ index.js
@@ -0,0 +1,13 @@
+const Manager = require("./library/Manager");
+const Engineer = require("./library/Engineer");
+const Intern = require("./library/Intern");
+const inquirer = require("inquirer");
+const path = require("path");
+const fs = require("fs");
+
+const OUTPUT_DIR = path.resolve(__dirname, "output");
+const outputPath = path.join(OUTPUT_DIR, "team.html");
+
+const render = require("./library/htmlMaker");
+
+const employees = []
