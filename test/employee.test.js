059edb1e2c9cdedc56bc0861c561948af7d8b6f4
Tigro <easterlingme95@gmail.com>
2023-02-25 15:37:40 -0600

    added inital employee test functions

---
+++ test/employee.test.js
@@ -0,0 +1,24 @@
+const Employee = require("../library/Employee");
+
+test("Can instantiate Employee instance", () => {
+  const e = new Employee();
+  expect(typeof(e)).toBe("object");
+});
+
+test("Can set name via constructor arguments", () => {
+  const name = "Alice";
+  const e = new Employee(name);
+  expect(e.name).toBe(name);
+});
+
+test("Can set id via constructor argument", () => {
+  const testValue = 100;
+  const e = new Employee("Foo", "employee" ,testValue);
+  expect(e.id).toBe(testValue);
+});
+
+test("Can set email via constructor argument", () => {
+  const testValue = "test@test.com";
+  const e = new Employee("Foo",'employee', 1, testValue);
+  expect(e.email).toBe(testValue);
+});
